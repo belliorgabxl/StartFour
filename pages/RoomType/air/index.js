2041cea6511cb49a49850f58e4db240e47e595ff
kasama07 <gasamatrang@gmail.com>
2024-02-21 17:30:02 +0700

    modified:   components/Styles/footer.module.css 	modified:   pages/RoomType/air/air.module.css 	modified:   pages/RoomType/air/index.js 	modified:   pages/RoomType/daily/daily.module.css 	modified:   pages/RoomType/daily/index.js 	modified:   pages/RoomType/fan/fan.module.css 	modified:   pages/RoomType/fan/index.js 	modified:   pages/RoomType/suite/index.js 	modified:   pages/RoomType/suite/suite.module.css

--- pages/RoomType/air/index.js
+++ pages/RoomType/air/index.js
@@ -36,29 +36,41 @@ export default function Home() {
             <div className={styles.container}></div>
             {dors?.dormitory?.map((d) => 
                 d.type === 'ห้องปรับอากาศ' && (
-                    <div key={d._id} >
-                          <Link  className={styles.boxgrid} href={"/Room/"+d._id} key={d._id}>
-                            
-                            <div className={styles.boxroomleft}>
-                                <div >
-                                    <img style={{borderRadius : '20px 0 0 20px'}} src={d.img} width={400} height={350} alt='img'></img>
-                                </div>
-                                <div className={styles.Text}>
-                                    <div className={styles.Texthead} >{d.dorm_name}</div>
-                                    <div className={styles.Textlocation}>{d.location}</div>
-                                    <div className={styles.Textdetail}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{d.detail}</div>
-                                    <div className={styles.icon3}>฿</div>
-                                    <div className={styles.price}>{d.price}</div>
-                                    <div className={styles.Textprice}>ราคาเริ่มต้น(ต่อเดือน)</div>
-                                    <div className={styles.Texttype}>{d.type}</div>
-                                    <img className={styles.iconstar} src="/star3.5.png" width={60} height={60}></img>
-                                    <img className={styles.iconmap} src="/map.png" width={20} height={20}></img>
-                                    <img className={styles.icondetail} src="/detail.png" width={20} height={20}></img>
-                                    <div className={styles.Textreview}>ดีมาก</div>
-                                </div>
-                            </div> 
-                        </Link>
-                    </div>   
+                  <div key={d._id} >
+                  <Link  className={styles.boxgrid} href={"/Room/"+d._id} key={d._id}>
+                      <div className={styles.boxroom}>
+                        <img style={{borderRadius : '15px 0 0 15px'}} src={d.img} width={400} height={350} alt='img'></img>
+                      </div> 
+                      <div className={styles.Text}>
+                              <div className={styles.line1}>
+                                <div className={styles.Texthead} >{d.dorm_name}</div>
+                              </div>
+                              <div className={styles.line2}>
+                                <img className={styles.iconstar} src="/star3.5.png" width={60} height={60}></img>
+                                <div className={styles.Textreview}>ดีมาก</div>
+                              </div>
+                              <div className={styles.line3}>
+                                <img className={styles.iconmap} src="/map.png" width={20} height={20}></img>
+                                <div className={styles.Textlocation}>{d.location}</div>
+                              </div>
+                              <div className={styles.line4}>
+                                <div className={styles.Texttype}>{d.type}</div>
+                              </div>
+                              <div className={styles.line5}>
+                                <img className={styles.icondetail} src="/detail.png" width={20} height={20}></img>
+                                <div className={styles.Textdetail}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{d.detail}</div>
+                              </div>
+                              <div className={styles.line6}>
+                                <div className={styles.icon3}>฿</div>
+                                <div className={styles.price}>{d.price}</div>
+                              </div>
+                              <div className={styles.line7}>
+                                <div className={styles.Textprice}>ราคาเริ่มต้น(ต่อเดือน)</div>
+                              </div>
+
+                          </div>
+                  </Link>
+              </div>    
                 )
             )}
             <Footer/>
