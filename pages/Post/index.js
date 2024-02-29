5c1fb65058a586702865dfd51c5be3b7c499a67c
belliorgabxl <64011224@kmitl.ac.th>
2024-02-29 22:52:36 +0700

    confix postroom css

--- pages/Post/index.js
+++ pages/Post/index.js
@@ -1,6 +1,6 @@
-import Navbar from "@/components/Navbar"
-import styles from "./post.module.css"
-import Footer from "@/components/Footer"
+import Navbar from "@/components/Navbar";
+import styles from "./post.module.css";
+import Footer from "@/components/Footer";
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 import Link from "next/link";
@@ -12,7 +12,7 @@ const getPost = async () => {
   return res.json();
 };
 
-export default function Post(){
+export default function Post() {
   const [post, setPost] = useState(null);
   useEffect(() => {
     getPost().then((p) => {
@@ -20,67 +20,49 @@ export default function Post(){
     });
   }, []);
 
-  return(
+  return (
     <div className={styles.body}>
-      <Navbar/>
+      <Navbar />
       <div className={styles.container}>
         <div className={styles.topic}>
           รวมห้องขายต่อไว้ที่นี่ แลกเปลี่ยนซื้อ-ขายสัญญาหอ
         </div>
         <div className={styles.roomPost}>
-          <div className={styles.topicPost}>
-            community post
-          </div>
-          {
-            post?.map((p)=>(
-             p.post==="yes"  && ( 
-             <Link key={p._id} href={"/Post/room/"+p._id}>
-                <div className={styles.roomBox}>
-                  <div className={styles.imgBox}>
-                    <img src={p.img} width={400} height={300} alt='img'></img>
-                  </div>
-                  <div className={styles.textBox}>
-                  <div className={styles.textBox1}>
-                    <div className={styles.dormNameBox}>
-                      {p.dorm_name}
+          <div className={styles.topicPost}>community post</div>
+          {post?.map(
+            (p) =>
+              p.post === "yes" && (
+                <Link key={p._id} href={"/Post/room/" + p._id}>
+                  <div className={styles.roomBox}>
+                    <div className={styles.imgBox}>
+                      <img src={p.img} width={350} height={270} alt="img"></img>
                     </div>
-                    <div className={styles.locationBox}>
-                      , {p.location}
-                    </div>
-                  </div> 
-                  <div className={styles.priceBox}>
-                    <div className={styles.price}>
-                      {p.price_m}
-                    </div>
-                    <div className={styles.month}>
-                      /month
-                    </div>
-                    <div className={styles.priceBox}>
-                      <div className={styles.priceLabel}>
-                      ค่าประกันหอพัก :
+                    <div className={styles.textBox}>
+                      <div className={styles.textBox1}>
+                        <div className={styles.dormNameBox}>{p.dorm_name}</div>
+                        <div className={styles.locationBox}>, {p.location}</div>
                       </div>
-                    <div className={styles.price}>
-                    {p.price_rent}
+                      <div className={styles.priceBox}>
+                        <div className={styles.price}>{p.price_m}</div>
+                        <div className={styles.month}>/month</div>
+                        <div className={styles.priceBox}>
+                          <div className={styles.priceLabel}>ประกัน :</div>
+                          <div className={styles.price}>{p.price_rent}</div>
+                          <div className={styles.month}>/month</div>
+                        </div>
+                      </div>
+                      <div className={styles.detailBox}>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{p.detail}
+                      </div>
+                      <div className={styles.typeBox}>Roomtype: {p.type}</div>
                     </div>
-                    <div className={styles.month}>
-                      /month
-                    </div>
-                    </div>
-                  </div> 
-                  <div className={styles.detailBox}>
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{p.detail}
-                  </div> 
-                  <div className={styles.typeBox}>
-                    Roomtype:    {p.type}
-                  </div> 
-          </div>
-        </div>
-            </Link>)
-            ))
-          }
+                  </div>
+                </Link>
+              )
+          )}
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
-  )
+  );
 }
