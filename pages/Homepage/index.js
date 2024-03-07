abb76f3d5db97218a61bcd38290f1c00cabfc6ff
belliorgabxl <64011224@kmitl.ac.th>
2024-03-08 02:21:02 +0700

    homepage components

--- pages/Homepage/index.js
+++ pages/Homepage/index.js
@@ -1,9 +1,9 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import Space from "@/components/SpaceTab";
-import styles from "./home.module.css"
+import styles from "./home.module.css";
 import Link from "next/link";
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 const getData = async () => {
   try {
@@ -13,85 +13,78 @@ const getData = async () => {
 
     if (!res.ok) {
       throw new Error("Failed to fetch topics");
-    }
-    else{
-      console.log("Information complete")
+    } else {
+      console.log("Information complete");
     }
     return res.json();
   } catch (error) {
     console.log("Error loading topics: ", error);
   }
 };
-export default function Homepage(){
+export default function Homepage() {
   const [dors, setDors] = useState(null);
   useEffect(() => {
     getData().then((d) => {
       setDors(d);
     });
   }, []);
-  return(
+  return (
     <div className={styles.body}>
-      <Navbar/>
+      <Navbar />
       <div className={styles.container}>
-
-      <div className={styles.topic}>
-      <h1>ค้นหาที่พักพร้อมสิทธิพิเศษสำหรับสมาชิก จากทั่วทุกมุม</h1> 
-      </div>
-      <div className={styles.room_function}>
-            <div className={styles.room_btn}>
-              <Link href="/">Top Range</Link>
-            </div>
-            <div className={styles.room_btn}>
-              <Link href="/">Suggest</Link>
-            </div>
-            <div></div>
-    </div>
-    
-      <div className={styles.roomcomponents}>
-        {dors?.dormitory?.map((d) =>
-        <Link href={'/Room/'+ d._id} key={d._id}>
-        <div className={styles.roomBox}>
-          <div className={styles.imgBox}>
-            <img src={d.img} width={400} height={300} alt='img'></img>
-          </div>
-          <div className={styles.textBox}>
-
-              <div className={styles.textBox1}>
-                <div className={styles.dormNameBox}>
-                  {d.dorm_name}
-                </div>
-                <div className={styles.locationBox}>
-                  , {d.location}
-                </div>
-              </div> 
-
-              <div className={styles.priceBox}>
-                <div className={styles.price}>
-                  {d.price}
-                </div>
-                <div className={styles.month}>
-                  /month
-                </div>
-              </div> 
-
-              <div className={styles.detailBox}>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{d.detail}
-              </div> 
-
-              <div className={styles.typeBox}>
-                Roomtype:    {d.type}
-              </div> 
-
-          </div>
-          
+        <div className={styles.topic}>
+          <h1>ค้นหาที่พักพร้อมสิทธิพิเศษสำหรับสมาชิก จากทั่วทุกมุม</h1>
         </div>
-    </Link>
-        )}
+        <div className={styles.room_function}>
+          <div className={styles.room_btn}>
+            <Link href="/">Top Range</Link>
+          </div>
+          <div className={styles.room_btn}>
+            <Link href="/">Suggest</Link>
+          </div>
+          <div></div>
+        </div>
 
+        <div className={styles.roomcomponents}>
+          {dors?.dormitory?.map((d) => (
+            <Link href={"/Room/" + d._id} key={d._id}>
+              <div className={styles.roomBox}>
+                <div className={styles.imgBox}>
+                  <img src={d.img} width={200} height={150} alt="img"></img>
+                </div>
+                <div className={styles.textBox}>
+                  <div className={styles.textBox1}>
+                    {d.dorm_name.length > 12 ? (
+                      <div className={styles.dormNameBox}>
+                        {d.dorm_name.slice(0, 10)}..
+                      </div>
+                    ) : (
+                      <div className={styles.dormNameBox}>{d.dorm_name}</div>
+                    )}
+
+                    <div className={styles.locationBox}>
+                      , {d.location.slice(0, 15)}...
+                    </div>
+                  </div>
+
+                  <div className={styles.priceBox}>
+                    <div className={styles.price}>{d.price}</div>
+                    <div className={styles.month}>/month</div>
+                  </div>
+
+                  <div className={styles.detailBox}>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{d.detail.slice(0, 60)}...
+                  </div>
+
+                  <div className={styles.typeBox}>Roomtype: {d.type}</div>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
-      </div>
-      <Space/>
-      <Footer/>
+      <Space />
+      <Footer />
     </div>
-  )
+  );
 }
